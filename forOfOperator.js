0d6f6fdce5dec4625822d53e0e4502879a214b3b
Gaurav Yadav <shany.gaurav@gmail.com>
2024-11-28 13:27:03 +0530

    forOfOperator.js

--- forOfOperator.js
+++ forOfOperator.js
@@ -1,3 +1,11 @@
 /**
  * For of operator
  */
+let arr = ['test', 'dataTest', 'valueTest'];
+// for(let i=0; i< arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+for(let val of arr) {
+    console.log(val);
+}
